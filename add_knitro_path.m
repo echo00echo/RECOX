bd3d2350d23f716cf3796b8cdebf863645faf805
Rui Yang <echo000echo@gmail.com>
2025-05-16 01:02:46 -0700

    Added two simulations for a paper draft

--- add_knitro_path.m
+++
@@ -1,7 +0,0 @@
-if ispc
-    addpath('C:\Program Files\Artelys\knitro-12.3.0-Win-64\knitromatlab');
-elseif isunix
-    addpath(strcat(getenv('KNITRO_HOME'),'/knitromatlab'));
-elseif ismac
-    error('add_knitro_path.m has not supported MacOS yet');
-end
